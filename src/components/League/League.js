b421ff0041b451b4a3358f0719a64871d990137f
bwilson19 <brendan.wilson19@gmail.com>
2020-02-24 17:49:35 -0800

    added tabs for league page

--- src/components/League/League.js
+++ src/components/League/League.js
@@ -9,7 +9,9 @@ import {
   Nav,
   ListGroup,
   Button,
-  Card
+  Card,
+  Table,
+  Image
 } from 'react-bootstrap';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
@@ -18,6 +20,7 @@ import './League.css';
 function League(props) {
   const [date, setDate] = useState(new Date());
   const [currentGames, setCurrentGames] = useState('');
+  const [currentPage, setCurrentPage] = useState('upcoming');
   const changeDate = date => {
     setDate(date);
     filterGames(date);
@@ -50,58 +53,131 @@ function League(props) {
       <Container id="leagueTabs">
         <Nav justify variant="pills" defaultActiveKey="link-0">
           <Nav.Item>
-            <Nav.Link eventKey="link-0">Upcoming Games</Nav.Link>
+            <Nav.Link
+              eventKey="link-0"
+              onClick={() => setCurrentPage('upcoming')}
+            >
+              Upcoming Games
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">Statistics</Nav.Link>
+            <Nav.Link eventKey="link-1" onClick={() => setCurrentPage('stats')}>
+              Statistics
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2">Rules</Nav.Link>
+            <Nav.Link eventKey="link-2" onClick={() => setCurrentPage('rules')}>
+              Rules
+            </Nav.Link>
           </Nav.Item>
         </Nav>
       </Container>
-      <Container>
-        <Row>
-          <Col lg={8}>
-            <Jumbotron fluid>
+      {currentPage === 'upcoming' && (
+        <Container>
+          <Row>
+            <Col lg={8}>
+              <Jumbotron fluid>
+                <Container align="center">
+                  <h1>Upcoming Games</h1>
+                  <Button onClick={() => setCurrentGames('')}>
+                    Show All Games
+                  </Button>
+                </Container>
+              </Jumbotron>
+              {!currentGames && (
+                <ListGroup>
+                  {games.map(game => (
+                    <Link to="/game" key={game.id}>
+                      <ListGroup.Item>{game.name}</ListGroup.Item>
+                    </Link>
+                  ))}
+                </ListGroup>
+              )}
+              {currentGames && (
+                <ListGroup>
+                  {currentGames.map(game => (
+                    <Link to="/game" key={game.id}>
+                      <ListGroup.Item>{game.name}</ListGroup.Item>
+                    </Link>
+                  ))}
+                </ListGroup>
+              )}
+            </Col>
+            <Col lg={4}>
               <Container align="center">
-                <h1>Upcoming Games</h1>
-                <Button onClick={() => setCurrentGames('')}>
-                  Show All Games
-                </Button>
+                <Card>
+                  <Card.Header as="h5">When do you want to play?</Card.Header>
+                  <Card.Body>
+                    <Calendar onChange={changeDate} value={date} />
+                  </Card.Body>
+                </Card>
               </Container>
-            </Jumbotron>
-            {!currentGames && (
-              <ListGroup>
-                {games.map(game => (
-                  <Link to="/game" key={game.id}>
-                    <ListGroup.Item>{game.name}</ListGroup.Item>
-                  </Link>
-                ))}
-              </ListGroup>
-            )}
-            {currentGames && (
-              <ListGroup>
-                {currentGames.map(game => (
-                  <Link to="/game" key={game.id}>
-                    <ListGroup.Item>{game.name}</ListGroup.Item>
-                  </Link>
-                ))}
-              </ListGroup>
-            )}
-          </Col>
-          <Col lg={4}>
-            <Container align="center">
-              <Card>
-                <Card.Header as="h5">When do you want to play?</Card.Header>
-                <Card.Body>
-                  <Calendar onChange={changeDate} value={date} />
-                </Card.Body>
-              </Card>
-            </Container>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {currentPage === 'stats' && (
+        <Container>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Table heading</th>
+                <th>Table heading</th>
+                <th>Table heading</th>
+                <th>Table heading</th>
+                <th>Table heading</th>
+                <th>Table heading</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+                <td>Table cell</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
+      )}
+      {currentPage === 'rules' && (
+        <Container>
+          <Image src="/images/rules.jpg" fluid />
+          <Card>
+            <Card.Header as="h5">Featured</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Container>
+      )}
     </>
   );
 }
